088f88c2cf738d4f53734f6fb05f2cfcafb65ad9
meissa10 <56763829+meissa10@users.noreply.github.com>
2022-06-15 20:07:32 +0000

    1st full working version

--- contact_page/my-app/src/App.js
+++ contact_page/my-app/src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
+import emailjs from 'emailjs-com';
 import './App.css';
 
 function App() {
+
+  function sendEmail(e){
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_labchrh",
+      "template_ib7wmy8",
+      e.target,
+      "RNFAKhLOEthkZqEbL"
+      ).then(res=>{
+          console.log(res);
+
+        }).catch(err =>{
+          console.log(err);
+        });
+      
+      e.target.reset();
+
+      alert("sent");
+      
+
+      
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contact Form</h1>
+      <form class="cf" onSubmit={sendEmail} >
+        <div class="half left cf">
+          <input type="text" name="name" placeholder="Name" />
+          <input type="email" name="email" placeholder="Email address"/>
+        </div>
+        <div class="half right cf">
+          <textarea name="message" type="text" id="message" placeholder="Message"></textarea>
+        </div>  
+        <input type="submit" value="Submit" id="input-submit"/>
+      </form>
+
+      
     </div>
   );
 }
 
 export default App;
+
+
+//CSS and HTNl from https://codepen.io/markmurray/pen/oNvpxw
